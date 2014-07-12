f5d5a935e1d088e7a32278a9314e9f52ac74a965
Wakkey <wakkeyuc@gmail.com>
2014-07-12 20:11:16 +0900

    update(式、変数、予約語、ブロックの分割)
    式、変数、値、予約語、ブロック別に分別、
    他方、式全体を見渡せるよう、式の流れを追える
    ように整形出来るようにした。

--- unit1.pas
+++ unit1.pas
@@ -13,11 +13,26 @@ type
 
   TForm1 = class(TForm)
     Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
     Edit1: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    Label7: TLabel;
     ListBox1: TListBox;
     ListBox2: TListBox;
     ListBox3: TListBox;
+    ListBox4: TListBox;
+    ListBox5: TListBox;
+    ListBox6: TListBox;
+    ListBox7: TListBox;
     procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
 
   private
     { private declarations }
@@ -33,6 +48,34 @@ implementation
 {$R *.lfm}
 
 { TForm1 }
+function replace_space(st:TStringList):boolean;
+  var
+    i,i1:integer;
+    sw:boolean;
+  begin
+    sw := true;
+    st.Text := StringReplace(st.Text,'　',' ',[rfReplaceAll]);
+    st.Text := StringReplace(st.Text,' ',char(13),[rfReplaceAll]);
+    if st.count > 1 then begin
+      i := st.Count -1;
+      if st.Text <> '' then
+      while i >= st.Count -1 do begin
+        if (st[i] = char(13)) or (st[i] = ' ') or (st[i] = '') or (st[i] = char(0)) then begin
+          st.Delete(i);
+        end;
+        dec(i);
+        if i = 0 then
+          exit;
+      end;
+    end;
+end;
+
+function ruthin0(s:string):string;
+begin
+  s := StringReplace(s,'　',' ',[rfReplaceAll]);
+  ruthin0 := StringReplace(s,' ',char(13),[rfReplaceAll]);
+end;
+
 function ruthin1:boolean;
 var
   i:integer;
@@ -67,10 +110,9 @@ var
       end;
     end;
   begin
-    setchar := 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz[]()#$"",."';
-    setnumber := '1234567890';
-    setcara := '-=~|\{+:*}/?\';
-    //sw_number := false;
+    setchar := 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz[]#$,.';
+    setnumber := '#@1234567890{}"''';
+    setcara := '-=~|\%+:*/!?\<>()';
     for i := 0 to length(setchar) -1 do begin
       if s = ' ' then begin
         sw1:=true;
@@ -143,29 +185,6 @@ var
   begin
     set_words(x,st1,st2,st3);
   end;
-
-  function replace_space(st:TStringList):boolean;
-  var
-    i,i1:integer;
-    sw:boolean;
-  begin
-    sw := true;
-    st.Text := StringReplace(st.Text,'　',' ',[rfReplaceAll]);
-    st.Text := StringReplace(st.Text,' ',char(13),[rfReplaceAll]);
-    if st.count > 1 then begin
-      i := st.Count -1;
-      if st.Text <> '' then
-      while i >= st.Count -1 do begin
-        if (st[i] = char(13)) or (st[i] = ' ') or (st[i] = '') or (st[i] = char(0)) then begin
-          st.Delete(i);
-        end;
-        dec(i);
-        if i = 0 then
-          exit;
-      end;
-    end;
-  end;
-
 begin
   s := form1.Edit1.Text;
   sw1 := false;
@@ -192,9 +211,103 @@ begin
   st3.Free;
 end;
 
+function ruthin2:boolean;
+var
+  i,i1:integer;
+  s,s1:string;
+  set_if:TstringList;
+  set_block:TstringList;
+  get_command1,get_command2:string;
+  get_if:TStringlist;
+  get_block:TstringList;
+  function get_ruthin(x:String):boolean;
+  var
+    i,i1,i2,i3:integer;
+    s:string;
+  begin
+    s:= x;
+    i2 := set_if.IndexOf(s);
+    if i2 > -1 then begin
+           get_command1 := set_if[i2];
+    end;
+       i3 := set_block.IndexOf(s);
+    if i3 > -1 then begin
+      get_command2 := set_block[i3];
+    end;
+  end;
+
+begin
+  set_if := TStringList.Create;
+  set_if.Text := 'IF' +char(13)+ 'if' +char(13)+'THEN'+char(13)+ 'then'+char(13);
+  set_block := TStringList.Create;
+  set_block.Text := 'BEGIN' +char(13)+'begin' +char(13)+'END' +char(13)+'end' +char(13)+'END;'+char(13)+ 'end;' +char(13)+';'+char(13);
+  get_if := TStringList.Create;
+  get_block := TStringList.Create;
+  get_if.Clear;
+  get_block.Clear;
+  s := ' ';
+
+  for i := 0 to form1.ListBox1.Count -1 do begin
+    get_command1 := '';
+    get_command2 := '';
+    get_ruthin(form1.listbox1.Items[i]);
+
+    s1 := '';
+    for i1 := 1 to length(get_command1) do begin
+      s1 := s1 + s[1];
+    end;
+    if get_command1 <> s1 then
+      get_if.Add(get_command1);
+    s1 := '';
+    for i1 := 1 to length(get_command2) do begin
+      s1 := s1 + s[1];
+    end;
+    if get_command2 <> s1 then
+      get_block.Add(get_command2);
+  end;
+  replace_space(get_if);
+  replace_space(get_block);
+
+  form1.ListBox4.Items.Text:= get_if.Text;
+  form1.ListBox5.Items.Text:= get_block.Text;
+
+end;
+
+function ruthin3(L1,L2:TStringList):boolean;
+var
+  i,i1:integer;
+begin
+  for i := 0 to l2.Count -1 do begin
+    i1 := l1.IndexOf(l2[i]);
+    if i1 > -1 then begin
+      l1.Delete(i1);
+    end;
+  end;
+end;
+
 procedure TForm1.Button1Click(Sender: TObject);
 begin
-  ruthin1
+  form1.ListBox7.Items.Text := ruthin0(edit1.Text);
+  ruthin1;
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  ruthin2
+end;
+
+procedure TForm1.Button3Click(Sender: TObject);
+var
+  l1,l2:TStringList;
+begin
+  l1 := TStringList.Create;
+  l2 := TStringList.Create;
+  l1.Text:= form1.ListBox1.Items.Text;
+  l2.Text:= form1.ListBox4.Items.Text;
+  ruthin3(l1,l2);
+  l2.Text:= form1.ListBox5.Items.Text;
+  ruthin3(l1,l2);
+  form1.ListBox6.Items.Text := l1.Text;
 end;
 
 
